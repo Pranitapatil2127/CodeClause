2a12018959dfd8dc968ce10baa25322c74750d01
Pranitapatil2127 <123476679+Pranitapatil2127@users.noreply.github.com>
2023-03-02 18:27:12 +0530

    Add files via upload

---
+++ PDF to WORD/PDF to WORD.py
@@ -0,0 +1,29 @@
+from pdf2docx import parse
+from typing import Tuple
+
+def convert_pdf2docs(input_file :str, output_file : str, pages: Tuple = None):
+
+    if pages:
+        pages = [int(i) for i in list(pages) if i.isnumeric()]
+    result = parse(pdf_file=input_file,
+                   docx_file= output_file, pages=pages)
+
+    summary = {
+        "File" : input_file, "Pages": str(pages), "Output File": output_file
+    }
+
+    print("## Summary #########################################################")
+    print("\n".join("{}:{}".format(i, j) for i , j in summary.items()))
+    print("#####################################################################")
+    return result
+
+if __name__ == "__main__":
+    import sys
+
+    #PDF File Name
+    input_file = "Pranita.pdf"
+
+    #What Name to Give After Converting PDF to Word
+    output_file = "123.docx"
+
+    convert_pdf2docs(input_file, output_file)
